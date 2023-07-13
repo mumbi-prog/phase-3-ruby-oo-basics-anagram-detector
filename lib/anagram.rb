3c1b7350f5b535114925080131fc90c47a086fa5
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-13 19:37:03 +0300

    Done with lab test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(match_word)
+      anagrams = []
+      match_word.each do |w|
+        if w.downcase.chars.sort == @word.chars.sort
+          anagrams.push w
+        end
+      end
+      anagrams
+    end
+end
+  
